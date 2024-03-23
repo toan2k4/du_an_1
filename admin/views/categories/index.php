9d465ab9a92baf3e4419601571f72b51d8ac14ff
toan2k4 <147464019+toan2k4@users.noreply.github.com>
2024-03-23 10:01:30 +0700

    category

---
+++ admin/views/categories/index.php
@@ -0,0 +1,62 @@
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">
+        <?= $title ?>
+    </h1>
+    <!-- <p class="mb-4">
+        DataTables is a third party plugin that is used to generate the
+        demo table below. For more information about DataTables, please
+        visit the
+        <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.
+    </p> -->
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <a href="<?= BASE_URL_ADMIN ?>?act=category-create" type="submit" class="btn btn-success">Thêm </a>
+
+        </div>
+        <div class="card-body">
+            <?php if (isset ($_SESSION['success'])): ?>
+                <div class="alert alert-success">
+                    <?= $_SESSION['success']?>
+                </div>
+                <?php unset($_SESSION['success'])?>
+            <?php endif; ?>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Tên danh mục</th>
+                            <th>Thao tác</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php foreach ($categories as $key => $value): ?>
+                            <tr>
+                                <td>
+                                    <?= $key + 1 ?>
+                                </td>
+                                <td>
+                                    <?= $value['ten_dm'] ?>
+                                </td>
+                                <td>
+                                    <a href="<?= BASE_URL_ADMIN ?>?act=category-detail&id=<?= $value['id'] ?>"
+                                        class="btn btn-info">Chi tiết</a>
+                                    <a href="<?= BASE_URL_ADMIN ?>?act=category-update&id=<?= $value['id'] ?>"
+                                        class="btn btn-warning">Sửa</a>
+                                    <a href="<?= BASE_URL_ADMIN ?>?act=category-delete&id=<?= $value['id'] ?>"
+                                        onclick="return confirm('Bạn có chắc chắn muốn xóa không')"
+                                        class="btn btn-danger">Xóa</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ admin/views/categories/update.php
@@ -0,0 +1,51 @@
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">
+        <?= $title ?>
+    </h1>
+    <!-- <p class="mb-4">
+        DataTables is a third party plugin that is used to generate the
+        demo table below. For more information about DataTables, please
+        visit the
+        <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.
+    </p> -->
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">
+                <?= $title ?>
+            </h6>
+        </div>
+        <div class="card-body">
+            <?php if (isset ($_SESSION['success'])): ?>
+                <div class="alert alert-success">
+                    <?= $_SESSION['success']?>
+                    <?php unset($_SESSION['success']) ?>
+                </div>
+            <?php endif; ?>
+            <?php if (isset ($_SESSION['errors'])): ?>
+                <div class="alert alert-danger">
+                    <ul>
+                        <?php foreach ($_SESSION['errors'] as $error): ?>
+                            <li>
+                                <?= $error ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php unset($_SESSION['errors']) ?>
+                </div>
+            <?php endif; ?>
+            <form action="" method="POST">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Tên danh mục</label>
+                    <input type="text" class="form-control" name="ten_dm" value="<?= $category['ten_dm'] ?>"
+                        placeholder="Nhập tên danh mục">
+                </div>
+
+                <input type="submit" name="gui" class="btn btn-success" value="Cập nhật">
+                <a href="<?= BASE_URL_ADMIN ?>?act=categories" type="submit" class="btn btn-warning">Quay lại </a>
+            </form>
+        </div>
+    </div>
+</div>
